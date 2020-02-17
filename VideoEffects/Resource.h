e1f8670fc00721d014d41dbf40e108588006b63a
prutskov <lehaprutskov@gmail.com>
2020-02-17 22:13:29 +0300

    Temp commit

--- VideoEffects/Resource.h
+++ VideoEffects/Resource.h
@@ -12,8 +12,8 @@
 #define IDC_RADIO_TBB                   1005
 #define IDC_COMBO_DEV                   1006
 #define IDC_OPEN_VIDEO                  1007
-#define IDC_OPEN_VIDEO2                 1008
 #define IDC_OPEN_CAMERA                 1008
+#define IDC_IMG_SIZE                    1009
 
 // Next default values for new objects
 // 
@@ -21,7 +21,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        130
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1007
+#define _APS_NEXT_CONTROL_VALUE         1010
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
