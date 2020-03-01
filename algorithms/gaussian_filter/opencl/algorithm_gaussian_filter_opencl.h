6de519bd5fdc0991cc40e43b651c3bcbf6dbc584
prutskov <lehaprutskov@gmail.com>
2020-03-02 00:54:14 +0300

    Added template of opencl gaussian filter

---
+++ algorithms/gaussian_filter/opencl/algorithm_gaussian_filter_opencl.h
@@ -0,0 +1,38 @@
+#pragma once
+#define CL_USE_DEPRECATED_OPENCL_1_2_APIS
+#define __CL_ENABLE_EXCEPTIONS
+#include <CL/cl.hpp>
+
+#include "../../Algorithm.h"
+#include "../parameter_gaussian_filter.h"
+
+namespace algorithms
+{
+	namespace gaussian_filter
+	{
+		namespace opencl
+		{
+			class Algorithm : public algorithms::Algorithm
+			{
+			public:
+				Algorithm();
+				float compute() override;
+				void setParameter(ParameterIface *parameter) override;
+				virtual ~Algorithm();
+			private:
+				void buildProgram();
+				void generateGaussianKernel();
+				void compute3x3();
+				void compute5x5();
+				std::vector<std::string> getDevices() override;
+
+				std::vector<cl::Platform> _platforms;
+				std::vector<cl::Device> _devices;
+				cl::Program _program;
+				cl::Context _context;
+			};
+		}
+	}
+}
+
+
